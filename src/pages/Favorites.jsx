11c479cf219bd5d0408605aad9b255053cc90861
gelso <gelso02@live.com>
2022-07-01 15:25:54 -0300

    req 01

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Favorites extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-favorites" />
+    );
+  }
+}
+export default Favorites;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile" />
+    );
+  }
+}
+export default Profile;
